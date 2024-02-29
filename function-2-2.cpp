ceaf1930616a47e7827b9c60ee44d0f9af96cd8e
DominicHuynh <phucvbin2005@gmail.com>
2024-02-29 22:18:31 +1030

    2-2

---
+++ function-2-2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int max_element(int array[], int n){
+    int max=array[0];
+    if(n<1){
+        return 0;
+    
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(max>array[i]){
+                max=array[i];
+            }
+        }
+
+    }
+    return max;
+}
---
+++ main-2-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+extern int max_element(int array[], int n);
+int main(){
+    int n=4;
+    int array[4]={1,2,3,4};
+    std::cout<<" The maximum number is: "<<max_element(array,4)<<std::endl;
+
+
+
+
+
+
+
+    return 0;
+}
