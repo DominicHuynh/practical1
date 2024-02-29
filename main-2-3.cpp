324a2bb8c7cf8262954c8d7141215a480ca305e1
DominicHuynh <phucvbin2005@gmail.com>
2024-02-29 23:00:51 +1030

    2-3

---
+++ main-2-3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+extern void two_five_nine(int array[], int n);
+int main(){
+    int n=4;
+    int array[9]={2,5,5,5,9,9,9,9,9};
+    two_five_nine(array,9);
+
+
+
+
+
+
+    return 0;
+}
