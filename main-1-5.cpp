6de2af0640f38f41e9708431752ea59c253433c0
DominicHuynh <phucvbin2005@gmail.com>
2024-02-29 17:17:36 +1030

    era again

---
+++ main-1-5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+extern int count_evens(int number);
+int main(){
+    int n=8;
+    std::cout<<"The number of even numbers is : "<<count_evens(8)<<std::endl;
+
+
+
+
+
+    return 0;
+}
