0eeb127f672887e2eb83665dd5149faa939379d5
DominicHuynh <phucvbin2005@gmail.com>
2024-02-29 17:35:31 +1030

    win streak

---
+++ function-1-6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int min_element(int array[], int n){
+    int min=array[0];
+    if(n<1){
+        return 0;
+    
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(min<array[i]){
+                min=array[i];
+            }
+        }
+
+    }
+    return min;
+}
---
+++ main-1-6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+extern int min_element(int array[], int n);
+int main(){
+    int n=4;
+    int array[4]={1,2,3,4};
+    std::cout<<" The smallest number is: "<<min_element(array,4)<<std::endl;
+
+
+
+
+
+
+
+    return 0;
+}
