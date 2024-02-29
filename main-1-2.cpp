c076aa1b7399899c6292781a2eea6af741ba1c97
DominicHuynh <phucvbin2005@gmail.com>
2024-02-29 16:17:38 +1030

    fixed

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,25 +1,10 @@
 #include<iostream>
-double array_mean(int array[], int n){
+extern double array_mean(int array[], int n);
     
 
-    int total=0;
-    if(n<1){
-        return 0.0;
-    }
-    else{
-        for(int i=0;i<n;i++){
-        total=total+array[i];
-        }
-    }
-    double avg=double (total*1.0/n);
-
-
-
-    return avg;
-}
 int main(){
-    int array[5]={1,2,3,4,9} ;
+    int array[0]={} ;
  
-    std::cout << "The average is : "<< array_mean(array,5)<< std::endl;
+    std::cout << "The average is : "<< array_mean(array,0)<< std::endl;
     return 0;
 }
