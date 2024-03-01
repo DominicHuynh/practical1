b124578854c6cb29b18cf85db3415ca4c5e2789e
DominicHuynh <phucvbin2005@gmail.com>
2024-03-02 00:00:07 +1030

    2-4

---
+++ function-2-4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+bool is_ascending(int array[], int n){
+    if (n<=0){
+        return false;
+    
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(array[i]>=array[i+1]){
+                return true;
+            }
+
+        }
+    }
+return true;
+
+
+
+
+}
---
+++ main-2-4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+extern bool is_ascending(int array[], int n);
+int main (){
+int n=4;
+int array[4]={1,2,3,4};
+ is_ascending( array,4);
+return 0;
+
+
+}
