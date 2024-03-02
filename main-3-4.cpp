ff1b416a6456b515b42864166ac988ac580b1bc1
DominicHuynh <phucvbin2005@gmail.com>
2024-03-02 13:44:02 +1030

    3-4

---
+++ main-3-4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+extern void print_pass_fail(char grade);
+int main(){
+    print_pass_fail('A'); 
+    print_pass_fail('B'); 
+    print_pass_fail('C'); 
+    print_pass_fail('D'); 
+    print_pass_fail('E'); 
+   
+
+
+
+    return 0;
+}
