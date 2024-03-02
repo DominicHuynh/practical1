a66f793f03c3f3c119b4ace8a45c2fe042fad932
DominicHuynh <phucvbin2005@gmail.com>
2024-03-02 14:08:56 +1030

    3-5

---
+++ function-3-5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ double sum_even(double array[], int n){
+    int sum=0;
+    if(n<1){
+        return 0;
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                sum=sum+array[i];
+            }
+        }
+    }
+    return sum;
+
+
+
+
+ }
---
+++ main-3-5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+extern double sum_even(double array[], int n);
+int main(){
+    int n=4;
+    double array[4]={1,2,3,4};
+    sum_even(array,4);
+
+
+
+
+
+    return 0;
+}
